bd4f1ff1f29e745e3cafb7da0cb549a6ed239434
RepoUpdater <repo-updater@example.com>
2026-02-13 21:15:06 +0530

    day-4 file handling

--- Week-2/Day-4/StudentRecords.py
+++ Week-2/Day-4/StudentRecords.py
@@ -1,10 +1,13 @@
+import os
 filename = input("Enter file name: ")
-with open(filename, "a") as f:
+path = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+with open(path, "a") as f:
     n = int(input("How many records? "))
     for i in range(n):
         name = input("Name: ")
         grade = input("Grade: ")
         f.write(name + "," + grade + "\n")
+
 print("\nAll Student Records:")
-with open(filename, "r") as f:
+with open(path, "r") as f:
     print(f.read())
