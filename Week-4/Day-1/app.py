2bc541bb076040e7333fca692aa8cb903c7c42a3
RepoUpdater <repo-updater@example.com>
2026-02-24 20:30:21 +0530

    internship

--- Week-4/Day-1/app.py
+++ Week-4/Day-1/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome! My Flask Internship Project is Live."
+    return "Welcome to the Flask app!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
