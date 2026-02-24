8100a920114af796d008098712607ed405c56ee5
RepoUpdater <repo-updater@example.com>
2026-02-24 20:12:33 +0530

    Added deployment files

--- Week-4/Day-1/app.py
+++ Week-4/Day-1/app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Welcome!! This is the home page of our Flask application."
+    return "Welcome! My Flask Internship Project is Live."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
